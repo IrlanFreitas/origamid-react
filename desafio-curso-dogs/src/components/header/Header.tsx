30f2f837a314d5d0205d395af6112aa5e4833eb0
Irlan Freitas <irlan.freitas@hotmail.com>
2025-09-24 11:51:54 -0300

    feat: criação do header

--- desafio-curso-dogs/src/components/header/Header.tsx
+++ desafio-curso-dogs/src/components/header/Header.tsx
@@ -1,11 +1,25 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
+import Dog from "../../assets/dogs.svg?react";
 
 const Header = () => {
   return (
-    <header className="container">
-      <NavLink to='/'>Home</NavLink>{" "}
-      <NavLink to='/login'>Login / Criar</NavLink>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <NavLink
+          className={styles.logo}
+          to="/"
+          style={{ marginRight: 20 }}
+          aria-label="Dogs - Home"
+        >
+          {/* <img src={Dog} alt="Icone de cachorro" /> */}
+          <Dog />
+        </NavLink>
+        {"   "}
+        <NavLink to="/login" className={styles.login}>
+          Login / Criar
+        </NavLink>
+      </nav>
     </header>
   );
 };
