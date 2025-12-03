a03f9448eb9f0e1d300bce620d06fb1583bf66c2
Irlan Freitas <irlan.freitas@hotmail.com>
2025-12-03 19:48:58 -0300

    style: ajuste de diagramação e estilização do feed inicial

--- desafio-curso-dogs/src/components/feedPhotosItem/FeedPhotosItem.tsx
+++ desafio-curso-dogs/src/components/feedPhotosItem/FeedPhotosItem.tsx
@@ -5,7 +5,7 @@ const FeedPhotosItem = ({ photo }: { photo: Photo }) => {
   return (
     <li className={styles.photo}>
       <img src={photo?.src} alt={photo.title} />
-      <span>{photo?.acessos}</span>
+      <span className={styles.views}>{photo?.acessos}</span>
       {/* <p>{photo?.title}</p>
       <p>{photo?.idade}</p>
       <p>{photo?.peso}</p>
